e15341b414a035298eb01909829fbcde8a43eddf
za-ibrahim <ibrahim@zeptoapps.com>
2025-05-31 18:53:12 +0600

    updated

--- src/components/Promot.jsx
+++ src/components/Promot.jsx
@@ -4,19 +4,19 @@ import ComponentLayout from '../layouts/ComponentLayout'
 
 const Promot = () => {
     return (
-        <div className='py-15 bg-secondary/30 pb-24'>
+        <div className='py-15 bg-accent/60 pb-24'>
             <ComponentLayout>
-                <Heading>Try our best Sign Customizer—built to keep you ahead</Heading>
+                <Heading><span className="text-background">Try our best Sign Customizer—built to keep you ahead</span></Heading>
                 <div className='flex sm:flex-row flex-col items-center mx-auto gap-4 pt-2 w-fit'>
                     <a href="https://apps.shopify.com/neon-sign-customizer" target="_blank" rel="noopener noreferrer">
-                        <button className='tryBtn group flex items-center justify-center shadow text-black !border !border-secondary/60 px-6 py-3 sm:text-base text-sm font-semibold rounded-full gap-2 transition-all duration-300'>
+                        <button className='tryBtn group flex items-center justify-center shadow text-black !border !border-secondary/80 px-6 py-3 sm:text-base text-sm font-semibold rounded-full gap-2 transition-all duration-300'>
                             <span>Create Your First Sign</span>
                             <span className='group-hover:translate-x-1 transition-all duration-300'>
                                 <svg xmlns="http://www.w3.org/2000/svg" fill='white' width="18" height="18" viewBox="0 0 18 18"><path d="M5.47 3.53 10.94 9 5.469 14.471l1.06 1.06L13.061 9 6.531 2.47z" /></svg>
                             </span>
                         </button>
                     </a>
-                    <button className='group w-fit flex justify-between items-center shadow gap-2 border border-background bg-transparent px-6 py-3 sm:text-base text-sm text-text font-semibold rounded-full transition-all duration-300'>
+                    <button className='group w-fit flex justify-between items-center shadow gap-2 border border-background bg-transparent px-6 py-3 sm:text-base text-sm text-background font-semibold rounded-full transition-all duration-300'>
                         <span>View Live Demo</span>
                         <span className='group-hover:translate-x-1 transition-all duration-300'>
                             <svg xmlns="http://www.w3.org/2000/svg" fill='black' width="18" height="18" viewBox="0 0 18 18"><path d="M5.47 3.53 10.94 9 5.469 14.471l1.06 1.06L13.061 9 6.531 2.47z" /></svg>

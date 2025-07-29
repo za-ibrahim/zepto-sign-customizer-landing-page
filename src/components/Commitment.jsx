1c4d7b085676753c9c36862d832037153e072fa8
za-ibrahim <ibrahim@zeptoapps.com>
2025-07-29 12:57:31 +0600

    update: commitment icon bg changed

--- src/components/Commitment.jsx
+++ src/components/Commitment.jsx
@@ -11,21 +11,25 @@ const Commitment = () => {
                         title="Theme-Friendly Integration"
                         description="Seamlessly integrate Zepto Sign Customizer with your existing store setup for smooth installation, easy transitions, and ongoing updates."
                         icon="./assets/Theme.svg"
+                        bg="bg-[#dd00ff]/20"
                     />
                     <CompCard
                         title="Designed for Simplicity"
                         description="Offering a sign customization experience doesn't have to be hard—Zepto Sign Customizer is simple to use yet impressively powerful."
                         icon="./assets/Customization.svg"
+                        bg="bg-[#0bb6fb]/20"
                     />
                     <CompCard
                         title="Zero-Code Required"
                         description="Zepto Sign Customizer requires no coding skills and offers a user-friendly experience that feels as intuitive as using your phone."
                         icon="./assets/No-Code.svg"
+                        bg="bg-[#f2b448]/20"
                     />
                     <CompCard
                         title="Support at Every Step"
                         description="Need help? Our chat and email support has you covered every step of the way."
                         icon="./assets/Community.svg"
+                        bg="bg-[#72cc57]/20"
                     />
                 </div>
             </ComponentLayout>
@@ -33,10 +37,10 @@ const Commitment = () => {
     )
 }
 
-const CompCard = ({ title, description, icon }) => {
+const CompCard = ({ title, description, icon, bg }) => {
     return (
         <div className='bg-background border border-primary/20 p-6 rounded-2xl flex lg:flex-row flex-col justify-between items-start gap-6'>
-            <div className='w-[85px] h-[85px] shrink-0 md:ml-0 mx-auto flex items-center justify-center bg-primary/20 rounded-full'>
+            <div className={`w-[85px] h-[85px] shrink-0 md:ml-0 mx-auto flex items-center justify-center ${bg} rounded-full`}>
                 <img className='w-10 h-10 p-0 m-0' src={icon} alt={title} />
             </div>
             <div className='flex flex-col gap-2 md:text-left text-center'>
